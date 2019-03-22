2abb368549ec88203f3ea81d1e177eb0088370b2
JuanC2017 <jhonlondon2015@gmail.com>
2019-03-22 11:25:57 -0500

    version-2

---
+++ models/ip.js
@@ -0,0 +1,12 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var IpSchema = Schema({
+    ip: { type: String, required: true },
+    conteo: { type: Number, required: true }
+    // Agregar date
+    // fec_cre: {type: Date, default: Date.now()}
+    // fec_upd: {type: Date, default: Date.now()}
+});
---
+++ models/recarga.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var RecaSchema = Schema({
+    numero: { type: String, required: true },
+    empresa: { type: String, required: true },
+    operador: { type: String, required: true },
+    monto: { type: String, required: true },
+    respuesta: { type: String, required: true },
+    fec_cre: { type: Date, default: Date.now() },
+    fec_upd: { type: Date, default: Date.now() }
+
+});
+
+module.exports = mongoose.model('Recarga', RecagaSchema);
